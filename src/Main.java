6b7103aabd925e82422669bb4b4578c013c82708
Juan Santos <juan_santostr@fet.edu.co>
2025-09-06 19:24:54 -0500

    Agregar la clase main

--- src/Main.java
+++ src/Main.java
@@ -40,6 +40,6 @@ public class Main {
         CuentaBancaria cuenta = new CuentaBancaria();
         cuenta.depositar(500);
         cuenta.retirar(200);
-        System.out.println("Saldo: " + cuenta.getSaldo()); // debe imprimir 300
+        System.out.println("Saldo: " + cuenta.getSaldo());
     }
 }
